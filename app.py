58db23fd5e3236588722932e4619477a89983249
ashutoshdebug <ashutoshkumart82@gmail.com>
2025-12-13 20:08:10 +0530

    Generalised

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 import requests
 from bs4 import BeautifulSoup
 
-website = requests.get("https://example.com")
-print(website.status_code)
+# website = requests.get("https://example.com")
+# print(website.status_code)
 
 # # Extracting text
 # response = website.text
@@ -17,9 +17,30 @@ print(website.status_code)
 # #     data = website.json()
 # #     print(data)
 
-if website.status_code == 200:
-    soup = BeautifulSoup(website.text, 'html.parser')
-    for tag in soup(['script', 'style', 'title']):
-        tag.decompose()
-    text = soup.get_text(separator = "\n", strip = True)
-    print(text)
+# if website.status_code == 200:
+#     soup = BeautifulSoup(website.text, 'html.parser')
+#     for tag in soup(['script', 'style', 'title']):
+#         tag.decompose()
+#     text = soup.get_text(separator = "\n", strip = True)
+#     print(text)
+
+# Generalising website url
+
+def webExtractor(url):
+    try:
+
+        website = requests.get(url)
+        website.raise_for_status()
+        soup = BeautifulSoup(website.text, 'html.parser')
+        for tag in soup(['script', 'style', 'title']):
+            tag.decompose()
+        text = soup.get_text(separator = "\n", strip = True)
+        print(f"\n{text}\n")
+
+    # Handling other status code
+    except requests.exceptions.RequestException as e:
+        print(f"Request error: \n{e}\n")
+
+
+url = input("Enter the url for website for text extraction: ")
+webExtractor(url)

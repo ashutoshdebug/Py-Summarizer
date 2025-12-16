a66b8248a8238eeb18fb758ed747eda8bf1a178e
ashutoshdebug <ashutoshkumart82@gmail.com>
2025-12-16 20:28:46 +0530

    Add selenium approach

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
-import requests
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 from bs4 import BeautifulSoup
+import time
+
 
 # website = requests.get("https://example.com")
 # print(website.status_code)
@@ -8,39 +12,42 @@ from bs4 import BeautifulSoup
 # response = website.text
 # # print(response)
 
-# # Parsing html
-# parsed_text = BeautifulSoup(response, 'html.parser')
-# for para in parsed_text:
-#     print(para.get_text())
-
-# # if website.status_code == 200:
-# #     data = website.json()
-# #     print(data)
-
-# if website.status_code == 200:
-#     soup = BeautifulSoup(website.text, 'html.parser')
-#     for tag in soup(['script', 'style', 'title']):
-#         tag.decompose()
-#     text = soup.get_text(separator = "\n", strip = True)
-#     print(text)
-
 # Generalising website url
 
+# def webExtractor(url):
+#     try:
+
+#         website = requests.get(url)
+#         website.raise_for_status()
+#         soup = BeautifulSoup(website.text, 'html.parser')
+#         for tag in soup(['script', 'style', 'title']):
+#             tag.decompose()
+#         text = soup.get_text(separator = "\n", strip = True)
+#         print(f"\n{text}\n")
+
+#     # Handling other status code
+#     except requests.exceptions.RequestException as e:
+#         print(f"Request error: \n{e}\n")
+
+
+# url = input("Enter the url for website for text extraction: ")
+# webExtractor(url)
+
 def webExtractor(url):
-    try:
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+    driver.get(url)
 
-        website = requests.get(url)
-        website.raise_for_status()
-        soup = BeautifulSoup(website.text, 'html.parser')
-        for tag in soup(['script', 'style', 'title']):
-            tag.decompose()
-        text = soup.get_text(separator = "\n", strip = True)
-        print(f"\n{text}\n")
+    # Wait for React to render
+    time.sleep(5)
 
-    # Handling other status code
-    except requests.exceptions.RequestException as e:
-        print(f"Request error: \n{e}\n")
+    soup = BeautifulSoup(driver.page_source, 'html.parser')
+    for tag in soup(['script', 'title', 'style']):
+        tag.decompose()
 
+    text = soup.get_text(separator = '\n', strip = True)
+    print(text)
 
-url = input("Enter the url for website for text extraction: ")
+    driver.quit()
+
+url = input("Enter the url: ")
 webExtractor(url)
